ad4164e729b580671135564ee24779b137bbe918
eeong <discography8@gmail.com>
2020-10-30 18:19:43 +0900

    201030 scroll

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,25 @@ function onSideClick(){
 	$(this).addClass("on");
 }
 
+var winHei = $(window).height();
+$("section").each(function(i){
+	$(this).attr("section-hei",i*winHei);
+});
+
+function onSectionWheel(e){
+
+	var nowPos = parseInt($(this).attr("section-hei"));
+	if (e.originalEvent.wheelDelta >= 0){
+		$("html, body").stop().animate({scrollTop: nowPos-winHei},800);
+		return false;
+	}
+	else if (e.originalEvent.wheelDelta < 0){
+		$("html, body").stop().animate({scrollTop: nowPos+winHei},800);
+		return false;
+	}
+}
+
 /************ EXECUTE ***********/ 
 $(".navi-icon").on("click",onNaviClick);
 $(".side-btn").on("click",onSideClick);
+$("section").on("mousewheel",onSectionWheel);
