fe7d26ac740e7c393fa95ebdbcd8b57978c0ab32
eeong <discography8@gmail.com>
2020-11-02 18:19:37 +0900

    201102-scroll, bg, deco

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,13 @@
 /************ COMMON ***********/ 
 
-
-
-
+var pagerNow = 0;
+var pagerLast = $(".side-navi .side-btn").length-1;
+var winHei = $(window).height();
 /************ FUNCTION ***********/ 
-
+function docInit(){
+	pagerAni(0);	
+	$("html").stop().animate({scrollTop: 0},500);
+}
 
 /************ CALLBACK ***********/ 
 function onNaviClick(){
@@ -12,29 +15,50 @@ function onNaviClick(){
 }
 
 function onSideClick(){
-	$(this).siblings().removeClass("on");
-	$(this).addClass("on");
+	pagerNow = $(this).index();
+	pagerAni(pagerNow);	
+	$("html").stop().animate({scrollTop: winHei*pagerNow},500);
 }
 
-var winHei = $(window).height();
-$("section").each(function(i){
-	$(this).attr("section-hei",i*winHei);
-});
+function pagerAni(pagerNow){
+	var $sideBtn = $(".side-navi .side-btn");
+	$sideBtn.removeClass("on");
+	$sideBtn.eq(pagerNow).addClass("on");
+}
 
 function onSectionWheel(e){
-
+	e.preventDefault();
+	e.stopPropagation();
+	$("section").each(function(i){
+	$(this).attr("section-hei",i*winHei);
+	});
 	var nowPos = parseInt($(this).attr("section-hei"));
-	if (e.originalEvent.wheelDelta >= 0){
-		$("html, body").stop().animate({scrollTop: nowPos-winHei},800);
+	if (e.originalEvent.wheelDelta > 0){
+		$("html").stop().animate({scrollTop: nowPos-winHei},400,function(){
+		pagerNow = pagerNow == 0 ? 0 : pagerNow -1;
+		pagerAni(pagerNow);
+		});
 		return false;
 	}
-	else if (e.originalEvent.wheelDelta < 0){
-		$("html, body").stop().animate({scrollTop: nowPos+winHei},800);
+		else if (e.originalEvent.wheelDelta < 0){
+		$("html").stop().animate({scrollTop: nowPos+winHei},400,function(){
+		pagerNow = pagerNow == pagerLast ? 4 : pagerNow + 1;
+		pagerAni(pagerNow);
+		});
 		return false;
-	}
+		}
+}
+
+
+
+function windowWheel(e){
+	e.preventDefault();
+	e.stopPropagation();
 }
 
 /************ EXECUTE ***********/ 
 $(".navi-icon").on("click",onNaviClick);
 $(".side-btn").on("click",onSideClick);
 $("section").on("mousewheel",onSectionWheel);
+$(window).on("mousewheel, scroll",windowWheel);
+$(window).on("load",docInit());
