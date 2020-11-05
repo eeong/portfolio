03330bfc8c67902c7f8f26ca2a2f847b44de91e7
eeong <discography8@gmail.com>
2020-11-05 18:20:38 +0900

    201105 slide

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,15 @@
 var pagerNow = 0;
 var pagerLast = $(".side-navi .side-btn").length-1;
 var winHei = $(window).height();
-
+var slideWid = $(".slide-left").outerWidth();
+var now = 1;
+var last = $(".front-slide-wrap").length - 1;
 /************ FUNCTION ***********/ 
 
 function docInit(){
 	pagerAni(0);	
 	$("html").stop().animate({scrollTop: 0},500);
+	$(".front-slide-wrap").stop().css({left: -slideWid*now+"px"});
 }
 
 /************ CALLBACK ***********/ 
@@ -37,15 +40,15 @@ function pagerAni(pagerNow){
 function onSectionWheel(e){
 	e.preventDefault();
 	e.stopPropagation();
+	console.log(e);
 	$("section").each(function(i){
 	$(this).attr("section-hei",i*winHei);
 	});
 	var nowPos = parseInt($(this).attr("section-hei"));
 	if (e.originalEvent.wheelDelta > 0){
-		$("html").stop().animate({scrollTop: nowPos-winHei},500,function(){
-		pagerNow = pagerNow == 0 ? 0 : pagerNow -1;
-		pagerAni(pagerNow);
-		
+			$("html").stop().animate({scrollTop: nowPos-winHei},500,function(){
+			pagerNow = pagerNow == 0 ? 0 : pagerNow -1;
+			pagerAni(pagerNow);
 		});
 		return false;
 	}
@@ -53,9 +56,26 @@ function onSectionWheel(e){
 		$("html").stop().animate({scrollTop: nowPos+winHei},500,function(){
 		pagerNow = pagerNow == pagerLast ? 4 : pagerNow + 1;
 		pagerAni(pagerNow);
+		$("section").removeClass("stopEvent");
 		});
 		return false;
 		}
+}
+function frontAni(now){
+	console.log(now);
+	$(".front-slide-wrap").stop().animate({left: slideWid*now+"px"});
+}
+
+function onFrontClickLeft(){
+	now = now == 0 ? last : now - 1; 
+	frontAni(now)
+	
+}
+
+function onFrontClickRight(){
+	now = last == last ? 0 : now + 1; 
+	frontAni(-now)
+	
 }
 
 
@@ -65,4 +85,5 @@ $(".navi li").on("click",onNaviClick)
 $(".side-btn").on("click",onSideClick);
 $("section").on("mousewheel",onSectionWheel);
 $(window).one("load", docInit());
-
+$(".front-end .bt-left").on("click",onFrontClickLeft);
+$(".front-end .bt-right").on("click",onFrontClickRight);
