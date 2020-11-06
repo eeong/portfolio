31c2c9ec210e4175e320939c3f70f4fcc948de39
eeong <discography8@gmail.com>
2020-11-06 18:19:16 +0900

    201106 front slide

--- public/js/index.js
+++ public/js/index.js
@@ -4,14 +4,14 @@ var pagerNow = 0;
 var pagerLast = $(".side-navi .side-btn").length-1;
 var winHei = $(window).height();
 var slideWid = $(".slide-left").outerWidth();
-var now = 1;
-var last = $(".front-slide-wrap").length - 1;
+var frontNow = 1;
+var frontLast = $(".front-slide").length ;
 /************ FUNCTION ***********/ 
 
 function docInit(){
 	pagerAni(0);	
 	$("html").stop().animate({scrollTop: 0},500);
-	$(".front-slide-wrap").stop().css({left: -slideWid*now+"px"});
+	$(".front-slide-wrap").stop().css({left: -slideWid*frontNow+"px"});
 }
 
 /************ CALLBACK ***********/ 
@@ -40,7 +40,6 @@ function pagerAni(pagerNow){
 function onSectionWheel(e){
 	e.preventDefault();
 	e.stopPropagation();
-	console.log(e);
 	$("section").each(function(i){
 	$(this).attr("section-hei",i*winHei);
 	});
@@ -56,26 +55,45 @@ function onSectionWheel(e){
 		$("html").stop().animate({scrollTop: nowPos+winHei},500,function(){
 		pagerNow = pagerNow == pagerLast ? 4 : pagerNow + 1;
 		pagerAni(pagerNow);
-		$("section").removeClass("stopEvent");
 		});
 		return false;
 		}
 }
-function frontAni(now){
-	console.log(now);
-	$(".front-slide-wrap").stop().animate({left: slideWid*now+"px"});
+function frontAni(a){
+	$(".front-slide-wrap").stop().animate({left: -slideWid*frontNow+"px"});
+}
+
+function frontInit(direc){
+	var $slideLeft = $(".front-slide").eq(0).clone();
+	var $slideCenter = $(".front-slide").eq(1).clone();
+	var $slideRight = $(".front-slide").eq(2).clone();
+	var frontSlide = [$slideLeft, $slideCenter, $slideRight];
+	if (direc == "left"){
+		$(".front-slide").eq(2).remove();
+		$(frontSlide[2]).prependTo(".front-slide-wrap");
+	}
+	else if(direc == "right"){
+		$(".front-slide").eq(0).remove();
+		$(frontSlide[0]).appendTo(".front-slide-wrap");
+	}
+	frontNow=1;
+	$(".front-slide-wrap").css({left: 0+"px"});
 }
 
 function onFrontClickLeft(){
-	now = now == 0 ? last : now - 1; 
-	frontAni(now)
-	
+	frontNow = frontNow == 0 ? 2 : frontNow - 1; 
+	frontAni()
+	$(".front-slide").eq(1).css({"flex":"33.3333% 0 0"}).children(".slide-image").css({"transform": "rotateY(0) scaleX(1) translateY(-60px)"});
+	$(".front-slide").eq(2).css("flex","16.6666% 0 0").children(".slide-image").css({"transform": "rotateY(20deg) scaleX(0.6) translateY(0)"});
+	frontInit('left')
 }
 
 function onFrontClickRight(){
-	now = last == last ? 0 : now + 1; 
-	frontAni(-now)
-	
+	frontNow = frontNow == 2 ? 0 : frontNow + 1; 
+	frontAni()
+	$(".front-slide").eq(2).css("flex","16.6666% 0 0").children(".slide-image").css({"transform": "rotateY(-20deg) scaleX(0.6) translateY(0)"});
+	$(".front-slide").eq(3).css({"flex":"33.3333% 0 0"}).children(".slide-image").css({"transform": "rotateY(0) scaleX(1) translateY(-60px)"});
+	frontInit('right')
 }
 
 
